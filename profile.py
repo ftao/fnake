537f12acc09bf1298694bd0d8078487f98acc4a3
ftao <Filia.Tao@gmail.com>
2011-12-18 09:12:32 +0800

    update profile tool

--- profile.py
+++ profile.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 import time
+import sys
 import pickle
+import pprint
 from agent import make_decision
 
 
-def test():
-    with open('live-1704.pickle', 'r') as f :
+def test(filename):
+    with open(filename, 'r') as f :
         data = pickle.load(f)
         start = time.time()
         seq = None
@@ -20,9 +22,11 @@ def test():
 #                snake['body'] = snake['body'][:1]
         #fake 
 #        data['info']['eggs'].append((16,4))
+        pprint.pprint(data['info']['snakes'])
+        print data['info']['snakes'][seq]['body']
         d = make_decision(data['map'], data['info'], seq)
         print 'd is ', d
         print 'cost', time.time() - start
 
 
-test()
+test(sys.argv[1])
